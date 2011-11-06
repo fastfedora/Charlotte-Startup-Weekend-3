7cfe9d387f77fa9423dee23ed84cb6b0dcfd7353
Trevor Lohrbeer <trevor@fastfedora.com>
2011-11-06 15:33:13 -0500

    Updated look & feel, added refresh to landing page

---
+++ themes/wificrowd/views/layouts/main.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html> <!-- The new doctype -->
+<html>
+    <head>
+    
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        
+		<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+        
+		<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" media="screen, projection" />
+		<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/form.css" />
+        <!-- Internet Explorer HTML5 enabling code: -->
+        
+        <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        
+        <style type="text/css">
+        .clear {
+          zoom: 1;
+          display: block;
+        }
+        </style>
+
+        
+        <![endif]-->
+        
+    </head>
+    
+    <body>
+    	
+    	<section id="page"> <!-- Defining the #page section with the section tag -->
+    
+            <header> <!-- Defining the header section of the page with the appropriate tag -->
+            
+                <a href="/"><img id="logo" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/logo-small.png" /></a>
+                                
+                <nav id="mainmenu"> <!-- The nav link semantically marks your main site navigation -->
+						<?php $this->widget('zii.widgets.CMenu',array(
+							'items'=>array(
+								array('label'=>'Home', 'url'=>array('/site/index')),
+								array('label'=>'Portals', 'url'=>array('/portal')),
+								array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+								array('label'=>'Logout', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+							),
+						)); ?>
+
+                </nav>
+            </header>
+
+            <section id="articles"> <!-- A new section with the articles -->
+				<!-- Article 1 start -->
+                <article id="article"> <!-- The new article tag. The id is supplied so it can be scrolled into view. -->
+                    <?php $this->widget('zii.widgets.CMenu', array('items'=>$this->menu, 'id'=>'toolbar', 'lastItemCssClass'=>'lastItem')); ?>
+
+                    <?php $this->widget('zii.widgets.CBreadcrumbs', array(
+                        'links'=>$this->breadcrumbs,
+                    )); ?><!-- breadcrumbs -->
+                    
+                    <div class="articleBody clear">
+						<?php echo $content; ?>
+                    </div>
+                </article>
+				<!-- Article end -->
+            </section>
+
+        <footer> <!-- Marking the footer section -->
+           <p>Copyright 2011 - WiFi Crowd, Inc.</p> <!-- Change the copyright notice -->
+        </footer>
+		</section> <!-- Closing the #page section -->
+    </body>
+</html>
